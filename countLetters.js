e38e4accddaebae7d011559bc6d23efc5cd41191
Sajan Thiara <sajan4@hotmail.com>
2021-03-03 22:45:05 +0000

    counting letters exercise

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+let countLetters = function(sentence) {
+  if (typeof sentence === 'string') {
+    let returnCount = {};
+    for (let letter of sentence) {
+      if (returnCount[letter]) {
+        returnCount[letter] += 1;
+      } else {
+        returnCount[letter] = 1;
+      }
+    }
+    return returnCount;
+  }
+};
+
+console.log(countLetters("this is a longer test, I forgot not to count spaces"));
