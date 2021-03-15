4c4feead954ed4c5ae6eeff9d37be875ff62b267
Sajan Thiara <sajan4@hotmail.com>
2021-03-15 04:13:33 +0000

    flatten refactoring

--- flatten.js
+++ flatten.js
@@ -10,45 +10,37 @@ let eqArrays = function(arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
     return false;
   }
-  for (i = 0; i < arrayOne.length; i++) {
+  for (let i = 0; i < arrayOne.length; i++) {
     if (arrayOne[i] !== arrayTwo[i]) {
       return false;
     }
   }
   return true;
-}
+};
 
 const flatten = array => {
-  let initArray = [];
+  let initArray = [...array];
   let midArray = [];
   let finalArray = [];
 
-  for (let thing of array) {
-    initArray.push(thing);
-  }
-
-  const flattenForReal = array => {
-    if (!(array.length === 0)) { 
+  (function flattenForReal(array) {
+    if (array.length !== 0) {
       if (Array.isArray(array[0]) && array[0].length !== 0) {
-          flattenForReal(array[0]);
+        flattenForReal(array[0]);
       }
       midArray.push(array.shift());
       flattenForReal(array);
     }
-/*     if (array.length > 0) {
-      array.shift();
-      flattenForReal(array);   
-    } */
-  }
-    
-  // if (initArray.length > 0) {
-    flattenForReal(initArray);
-  // }
+  }(initArray));
 
-  midArray.forEach(index => {if (!Array.isArray(index)) {finalArray.push(index)}});
+  midArray.forEach(index => {
+    if (!Array.isArray(index)) {
+      finalArray.push(index);
+    }
+  });
 
   return finalArray;
-}
+};
 
 assertArraysEqual(flatten([[], [1, [1]], [], [1, [1], [[[[[1]]]]]], [1, 1, 1], [[]], []]), [1, 1, 1, 1, 1, 1, 1, 1]);
 assertArraysEqual(flatten([1]), [1]);
